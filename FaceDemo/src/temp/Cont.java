0c3f90ddce99b1020d8ab304824304a441c11be7
Pushkkaarr <pushkarmmhatre3090@gmail.com>
2024-03-30 13:06:30 +0530

    changes in scan_face file to compare faces

--- FaceDemo/src/temp/Cont.java
+++ FaceDemo/src/temp/Cont.java
@@ -19,7 +19,29 @@ public class Cont {
         }
         return statement;
     }
+    
+    public static byte[] getFaceImageFromDatabase(String userId) {
+        byte[] imageData = null;
 
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/face", "root", "MePushkar@sql#193?PW")) {
+            Statement statement = connection.createStatement();
+            String query = "SELECT FaceImage FROM face WHERE TeacherID = '" + userId + "'";
+
+            ResultSet resultSet = statement.executeQuery(query);
+
+            if (resultSet.next()) {
+                imageData = resultSet.getBytes("face_image");
+            }
+
+            resultSet.close();
+            statement.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return imageData;
+    }
     public static ResultSet getResultset(String query) throws SQLException {
         statement = getStatement();
         ResultSet rs = statement.executeQuery(query);
--- FaceDemo/src/temp/scan_face.java
+++ FaceDemo/src/temp/scan_face.java
@@ -1,11 +1,12 @@
 package temp;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfRect;
@@ -19,67 +20,108 @@ import org.opencv.videoio.VideoCapture;
 
 
 public class scan_face extends javax.swing.JFrame  {
-    private DaemonThread myThread = null;
-    int count = 0;
-    VideoCapture webSource = null;
-    Mat frame = new Mat();
-    MatOfByte mem = new MatOfByte();
-    CascadeClassifier faceDetector = new CascadeClassifier("E:/Softwares/NetBeans IDE/Projects/Github/Face-Detection-Attendance-System/FaceDemo/src/temp/haarcascade_frontalface_default.xml");
-    MatOfRect faceDetections = new MatOfRect();
-   String Id1;
-    class DaemonThread implements Runnable {
-
-        protected volatile boolean runnable = false;
-
-        @Override
-        public void run() {
-            synchronized (this) {
-                while (runnable) {
-                    if (webSource.grab()) {
-                        try {
-                            //webSource.setPreferredSize(new Dimension(300, 250));
-                            webSource.retrieve(frame);
-                            Graphics g = jPanel1.getGraphics();
-                            faceDetector.detectMultiScale(frame, faceDetections);
-                            for (Rect rect : faceDetections.toArray()) {
-                               // System.out.println("ttt");
-                                Imgproc.rectangle(frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
-                                        new Scalar(0, 255,0));
-                            }
-                           Imgcodecs.imencode(".bmp", frame, mem);
-                            Image im = ImageIO.read(new ByteArrayInputStream(mem.toArray()));
-                            BufferedImage buff = (BufferedImage) im;
-                            if (g.drawImage(buff, 0, 0, getWidth(), getHeight()-150 , 0, 0, buff.getWidth(), buff.getHeight(), null)) {
-                                if (runnable == false) {
-                                    System.out.println("Paused ..... ");
-                                    this.wait();
-                                }
-                            }
-                        } catch (Exception ex) {
-                            System.out.println("Error!!");
-                            ex.printStackTrace();
-                        }
-                    }
-                }
-            }
-        }
-    }
-    public scan_face() {
+     String Id1;
+       public scan_face() {
         initComponents();
          System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // Load OpenCV library
-       // this.setExtendedState(MAXIMIZED_BOTH);
-         this.setTitle("Face Recognition");
-         this.setBounds(200,100,300,300);
+          this.setTitle("Scannig User Face");
+         this.setBounds(300,200,800,600);
 
     }
    public scan_face(String str){
        Id1=str;
        initComponents();
          System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // Load OpenCV library
-        //this.setExtendedState(MAXIMIZED_BOTH);
+       
          this.setTitle("Scannig User Face");
-         this.setBounds(200,100,300,300);
+         this.setBounds(300,200,800,600);
    }
+    
+    private DaemonThread myThread = null;
+    VideoCapture webSource = null;
+    Mat frame = new Mat();
+    MatOfByte mem = new MatOfByte();
+    CascadeClassifier faceDetector = new CascadeClassifier("E:/Softwares/NetBeans IDE/Projects/Github/Face-Detection-Attendance-System/FaceDemo/src/temp/haarcascade_frontalface_default.xml");
+    MatOfRect faceDetections = new MatOfRect();
+  
+
+
+class DaemonThread implements Runnable {
+
+    protected volatile boolean runnable = false;
+    private Mat storedFace;
+    private byte[] databaseFaceData;
+    private JPanel jPanel;
+
+    DaemonThread(Mat storedFace, byte[] databaseFaceData, JPanel jPanel) {
+        this.storedFace = storedFace;
+        this.databaseFaceData = databaseFaceData;
+        this.jPanel = jPanel;
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            while (runnable) {
+                if (webSource.grab()) {
+                    try {
+                        webSource.retrieve(frame);
+
+                        // Detect faces
+                        faceDetector.detectMultiScale(frame, faceDetections);
+
+                        // Draw detection results
+                        for (Rect rect : faceDetections.toArray()) {
+                            Imgproc.rectangle(frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+                                    new Scalar(0, 255,0));
+                        }
+
+                        // Display the frame
+                        Image im = ImageIO.read(new ByteArrayInputStream(mem.toArray()));
+                        BufferedImage buff = (BufferedImage) im;
+                        Graphics g = jPanel.getGraphics();
+                        g.drawImage(buff, 0, 0, jPanel.getWidth(), jPanel.getHeight(), 0, 0, buff.getWidth(), buff.getHeight(), null);
+
+                        if (runnable == false) {
+                            System.out.println("Paused ..... ");
+                            this.wait();
+                        }
+                    } catch (Exception ex) {
+                        System.out.println("Error!!");
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
+
+public static double compareFaces(byte[] img1Data, Mat img2) {
+    Mat img1 = Imgcodecs.imdecode(new MatOfByte(img1Data), Imgcodecs.IMREAD_GRAYSCALE);
+
+    if (img1.empty()) {
+        throw new IllegalArgumentException("Failed to decode image 1");
+    }
+
+    Mat img1Gray = new Mat();
+    Mat img2Gray = new Mat();
+
+Imgproc.cvtColor(img1, img1Gray, Imgproc.COLOR_BGR2GRAY);
+    Imgproc.cvtColor(img2, img2Gray, Imgproc.COLOR_BGR2GRAY);
+
+    int matchMethod = Imgproc.TM_CCOEFF_NORMED;
+
+    Mat result = new Mat();
+    Imgproc.matchTemplate(img1Gray, img2Gray, result, matchMethod);
+
+    Core.MinMaxLocResult minMaxLocResult = Core.minMaxLoc(result);
+    double similarity = minMaxLocResult.maxVal;
+
+    return similarity;
+}
+    
+    
+ 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -118,26 +160,26 @@ public class scan_face extends javax.swing.JFrame  {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(156, 156, 156)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 315, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(218, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(117, 117, 117)
-                .addComponent(jButton1)
+                .addGap(116, 116, 116)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(125, 125, 125))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(192, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 315, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(182, 182, 182))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(24, 24, 24)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 293, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 304, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addGap(85, 85, 85))
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(64, 64, 64))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -155,9 +197,24 @@ public class scan_face extends javax.swing.JFrame  {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-   webSource = new VideoCapture(0); // video capture from default cam
-        myThread = new DaemonThread(); //create object of threat class
+        webSource = new VideoCapture(0); // video capture from default cam
+        byte[] databaseFaceData = Cont.getFaceImageFromDatabase("id");
+         Mat storedFace = new Mat(1, databaseFaceData.length, CvType.CV_8U);
+          storedFace.put(0, 0, databaseFaceData);
+
+        myThread = new DaemonThread(storedFace, databaseFaceData, jPanel1);
+     
         Thread t = new Thread(myThread);
+       
+
+    if (databaseFaceData != null && databaseFaceData.length > 0) {
+        var databaseFace = new Mat(1, databaseFaceData.length, CvType.CV_8U);
+        databaseFace.put(0, 0, databaseFaceData);
+
+        myThread = new DaemonThread(storedFace,databaseFaceData, jPanel1);
+    } else {
+        System.err.println("No image data found for the specified ID.");
+    }
         t.setDaemon(true);
         myThread.runnable = true;
         t.start();                 //start thrad
